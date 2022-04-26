b21baf4eceac4144a76aeacf8ae8aba92f8e8720
Alain Olivier <oliviera9@gmail.com>
2022-04-26 18:24:23 +0200

    refactor(ptokens-helpers): fix types in http-utils functions

--- packages/ptokens-helpers/src/http/http-utils.ts
+++ packages/ptokens-helpers/src/http/http-utils.ts
@@ -1,7 +1,7 @@
 import fetch, { HeadersInit, Response } from 'node-fetch'
 import { ERROR_JSON_RESPONSE_EXTRACTION } from '../errors'
 
-export const postRequest = async (_url: string, _body: unknown, _headers: HeadersInit = {}, _timeout = 2000) => {
+export async function postRequest(_url: string, _body, _headers: HeadersInit | undefined = {}, _timeout = 2000) {
   console.debug(`Outgoing POST ${_url}`)
   const controller = new AbortController()
   const timeout = setTimeout(() => {
@@ -24,7 +24,7 @@ export const postRequest = async (_url: string, _body: unknown, _headers: Header
   }
 }
 
-export const getRequest = async (_url: string, _headers = {}, _timeout = 2000) => {
+export async function getRequest(_url: string, _headers: HeadersInit | undefined = {}, _timeout = 2000) {
   console.debug(`Outgoing GET ${_url}`)
   const controller = new AbortController()
   const timeout = setTimeout(() => {
@@ -45,16 +45,28 @@ export const getRequest = async (_url: string, _headers = {}, _timeout = 2000) =
   }
 }
 
-export const getJsonBody = async (_result: Response) => {
+export async function getJsonBody<T>(_result: Response): Promise<T> {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return await _result.json()
+    return (await _result.json()) as T
   } catch {
     throw new Error(ERROR_JSON_RESPONSE_EXTRACTION + JSON.stringify(_result))
   }
 }
-export const fetchJsonByGet = (_url: string, _headers = {}, _timeout = 50000): Promise<any> =>
-  getRequest(_url, _headers, _timeout).then(getJsonBody)
+export async function fetchJsonByGet<T>(
+  _url: string,
+  _headers: HeadersInit | undefined = {},
+  _timeout = 50000
+): Promise<T> {
+  const resp = await getRequest(_url, _headers, _timeout)
+  return await getJsonBody<T>(resp)
+}
 
-export const fetchJsonByPost = (_url: string, _body, _headers = {}, _timeout = 500) =>
-  postRequest(_url, _body, _headers, _timeout).then(getJsonBody)
+export async function fetchJsonByPost<T>(
+  _url: string,
+  _body,
+  _headers: HeadersInit | undefined = {},
+  _timeout = 500
+): Promise<T> {
+  const resp = await postRequest(_url, _body, _headers, _timeout)
+  return await getJsonBody<T>(resp)
+}
